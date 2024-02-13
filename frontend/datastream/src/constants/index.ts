3c548ee4c5febc7a43e82e6d30f48c7297a470dc
Ayomitide OAJ <ayomitide007@gmail.com>
2024-02-13 13:53:59 +0100

    chore: destructing

--- frontend/datastream/src/constants/index.ts
+++ frontend/datastream/src/constants/index.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/projects';
+export const projectId = 'b7823232-81a9-4cd8-a3fc-63dda206d63f';
--- frontend/datastream/src/pages/index.tsx
+++ frontend/datastream/src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Project from '../components/Project';
+import { projectId } from '../constants';
 
 const HomePage = () => {
   return (
     <div className="container mx-auto px-4">
-      <Project projectId="b7823232-81a9-4cd8-a3fc-63dda206d63f" />
+      <Project projectId = {projectId} />
     </div>
   );
 };
